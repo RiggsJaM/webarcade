b7bd7d9b2b93aff47f79eb3c26c08f138fa9b854
John Hatton <hattonjd@iu.edu>
2022-09-08 23:02:20 -0400

    Implement Xamarin App demo
    Xamarin is a cross-platform toolset from Microsoft. I followed a basic
    template from Microsoft, and created an Android App. It just has a few
    lines of text, and a custom PNG file

---
+++ src/BillSplitter/BillSplitter/BillSplitter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BillSplitter.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BillSplitter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
