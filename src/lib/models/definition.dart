2803c21aab470ba696113d5dcbf46fcddabdf80a
John Hatton <hattonjd@iu.edu>
2022-09-20 17:02:57 -0400

    Add Dictionary.dart, and corresponding files.
    I attempted this this morning, but was in a rush, and did
    git add -u, instead of manually adding the files I had added.
    I made sure to do a git merge, to get the newest changes, before
    uploading this.

---
+++ src/lib/models/definition.dart
@@ -0,0 +1,21 @@
+class Definition {
+  final String ref; // Which MW Api
+  final String word;
+  final String def;
+  final String functionLabel;
+
+  const Definition({
+    required this.ref,
+    required this.word,
+    required this.def,
+    required this.functionLabel});
+
+  factory Definition.fromJson(Map<String, dynamic> json) {
+    return Definition(
+      word: json['word'],
+      ref: json['ref'],
+      def: json['def'],
+      functionLabel: json['fl'],
+    );
+  }
+}
