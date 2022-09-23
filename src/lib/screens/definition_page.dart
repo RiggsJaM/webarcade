3beef94846b7115783b9484ba34e7248e5dd179e
John Hatton <hattonjd@iu.edu>
2022-09-23 16:36:55 -0400

    Implement basic screen for the Definition Page

--- src/lib/screens/definition_page.dart
+++ src/lib/screens/definition_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:retro_arcade/models/definition_model.dart';
 import 'package:retro_arcade/mw_key.dart';
@@ -20,61 +22,9 @@ Future<Welcome> fetchWelcome(http.Client client, String myWord) async {
     // If the server did return a 200 OK response,
     // then parse the JSON.
 
-    // var objectFromJson = jsonDecode(response.body);
-    // // objectFromJson is a LIST! We want to map it properly.
-    // // The list is of each definition. Inside each definition are the fields we want.
-    // // The debugPrint should help visualize this.
-    // List<dynamic> list = jsonDecode(response.body);
-    // for (int i=0; i < list.length; i++)
-    //   {
-    //     debugPrint('${list[i]}\n');
-    //   }
-    // // Goes boom here... fromJson expects Map, but objectFromJson is a List...
-    // var wordFromJson = Word.fromJson(objectFromJson);
-    //
-    // debugPrint('The runtime object type of the object we\'re trying to pass is: \n${wordFromJson.runtimeType}\n');
-    //
-    // return Word.fromJson(json.decode(response.body));
-
-    // // Step 1, get a List<dynamic>
-    // List<dynamic> responseBody = jsonDecode(response.body.toString());
-    //
-    //
-    // debugPrint("Hello?"); // Still goes boom here.
-    // // The following line of code produces the following runtime error:
-    // // Expected Property Name or '}' in JSON at position 2
-    //
-    //
-    //
-    //
-    // debugPrint('The runtime object type of the object we\'re trying to pass is: \n${definitionsList.runtimeType}\n');
-    // debugPrint('About to print responseBody.toString() contents\n\n');
-    // debugPrint('${definitionsList}');
-    //
-    //
-    // //
-    // //List<dynamic> metaList
-    //
-    // Map<String,dynamic> parsed = jsonDecode(responseBody.toString()).cast<Map<String, dynamic>>();
-    //
-    //
-    // debugPrint('It\'s contents are: \n${parsed}');
-    // debugPrint("Servus!");
-    //
-    // var map = responseBody.first;
-    // Record parsedList = Record.fromJson(map);
-    //
-    // debugPrint('The runtime object type of the object we\'re trying to pass is: \n${parsed.runtimeType}\n');
-    //
-    // //final retVal = parsed.map<Record>((json) => Record.fromJson(json)).toList();
-
-    // We have a List<dynamic> that we want
-
-
     final welcomeList = welcomeFromJson(response.body);
-
-
-    final firstDefMap = welcomeList.first;
+    final firstDefMap = welcomeList[0];
+    final secondDefMap = welcomeList[1];
 
     return firstDefMap;
   } else {
@@ -97,11 +47,22 @@ class DefinitionPage extends StatefulWidget {
 class _DefinitionPage extends State<DefinitionPage> {
 
   late Future<Welcome> futureWelcome;
+  // late TextEditingController _wordController;
+  // late TextEditingController _definitionController;
+  // late TextEditingController _urlController;
+
+  String _responseBody = '<empty>';
+  String _error = '<none>';
+  bool _pending = false;
 
   @override
   void initState() {
     super.initState();
     futureWelcome = fetchWelcome(http.Client(), "flutter"); // Added http.Client() for testing purposes
+
+    // this._definitionController = TextEditingController();
+    // this._urlController = TextEditingController()
+    //   ..text = 'https://dictionaryapi.com/api/v3/references/collegiate/json/';
   }
 
   @override
@@ -119,18 +80,150 @@ class _DefinitionPage extends State<DefinitionPage> {
           child: FutureBuilder<Welcome>(
             future: futureWelcome,
             builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.shortdef!.first.toString()); // Should give the first definition in the list
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+            List<Widget> children;
+
+            if (snapshot.hasData) {
+              children = <Widget>[
+                const Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.green,
+                  size: 60,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text("Flutter: \n"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text(snapshot.data!.shortdef!.first.toString()),
+                ),
+              ];
+            } else if (snapshot.hasError) {
+              children = <Widget>[
+                const Icon(
+                  Icons.error_outline,
+                  color: Colors.red,
+                  size: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Text('Error: ${snapshot.error}'),
+                ),
+              ];
+            } else {
+              children = const <Widget>[
+                SizedBox(
+                  width: 60,
+                  height: 60,
+                  child: CircularProgressIndicator(),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Text('Awaiting result...'),
+                ),
+              ];
+            }
 
               // By default, show a loading spinner.
-              return const CircularProgressIndicator();
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                )
+              );
             },
           ),
         ),
       ),
     );
+  //   return ListView(
+  //     padding: const EdgeInsets.all(16.0),
+  //     children: <Widget>[
+  //       TextField(
+  //         controller: this._wordController,
+  //         decoration: const InputDecoration(
+  //           labelText: 'URL to GET',
+  //           border: OutlineInputBorder(),
+  //         ),
+  //       ),
+  //       const SizedBox(height: 4),
+  //       TextField(
+  //         controller: this._definitionController,
+  //         decoration: const InputDecoration(
+  //           labelText: 'Optional api token',
+  //           border: OutlineInputBorder(),
+  //         ),
+  //       ),
+  //       ButtonBar(
+  //         children: <Widget>[
+  //           ElevatedButton(
+  //             onPressed: _pending
+  //                 ? null
+  //                 : () =>
+  //                 _welcomeFetch(http.Client(), _wordController.text,
+  //                     _definitionController.text),
+  //             child: const Text('Get'),
+  //           ),
+  //           ElevatedButton(
+  //             onPressed: this._reset,
+  //             child: const Text('Reset'),
+  //           ),
+  //         ],
+  //       ),
+  //       Text('Response body=$_responseBody'),
+  //       const Divider(),
+  //       Text('Error=$_error'),
+  //     ],
+  //   );
+  // }
+  //
+  //
+  // void _reset({bool resetControllers = true}) {
+  //   setState(() {
+  //     if (resetControllers) {
+  //       this._urlController.text =
+  //       'https://dictionaryapi.com/api/v3/references/collegiate/json/';
+  //     }
+  //     this._responseBody = '<empty>';
+  //     this._error = '<none>';
+  //     this._pending = false;
+  //   });
+  // }
+  //
+  //
+  // Future<Welcome> _welcomeFetch(http.Client client, String word,
+  //     String text) async {
+  //   _reset();
+  //   setState(() => this._pending = true);
+  //
+  //   final String myUrl = '${_urlController.toString()}$word?key=${mw_apiKey}';
+  //   final response = await client.get(Uri.parse(myUrl));
+  //
+  //
+  //   if (response.statusCode == 200) {
+  //     final welcomeList = welcomeFromJson(response.body);
+  //     final firstDefMap = welcomeList[0];
+  //
+  //     return firstDefMap;
+  //   } else {
+  //     // If the server did not return a 200 OK response,
+  //     // then throw an exception.
+  //     throw Exception('Failed to load definition');
+  //   }
+
+    //   try {
+    //
+    //     // Usually we will add code to convert the response body into our data
+    //     // class, e.g. using https://javiercbk.github.io/json_to_dart/.
+    //     // **Tip**: use compute() function (from flutter/foundation.dart) to run
+    //     // heavy json parsing work in a background isolate.
+    //     final parsed = await compute(jsonDecode, response.body);
+    //     debugPrint('parsed json object=$parsed');
+    //     setState(() => this._responseBody = response.body);
+    //   } catch (e) {
+    //     setState(() => this._error = e.toString());
+    //   }
+    //   setState(() => this._pending = false);
+    // }
   }
 }
