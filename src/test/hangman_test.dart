8600979bfcb070fec0c9b6bae418f835e2f71632
Jacob Riggs <jarigg@iu.edu>
2022-10-11 22:03:10 -0400

    Fix other tests, 9/10 run now, but only temporary solution

--- src/test/hangman_test.dart
+++ src/test/hangman_test.dart
@@ -58,6 +58,7 @@ void main() {
       expect(find.text('E'), findsOneWidget);
   });
 
+  ///THESE TESTS ARE EXTREMELY TEMPORARY, THEY WILL ONLY WORK WITH THE WORD TO GUESS BEING TEST
   testWidgets("Gallows shows up on the page", (WidgetTester tester) async {
       await tester.pumpWidget(new Hangman());
       await tester.pumpAndSettle();
@@ -77,73 +78,73 @@ void main() {
   testWidgets("Body shows up on the page", (WidgetTester tester) async {
     await tester.pumpWidget(new Hangman());
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
 
     expect(find.image(const AssetImage("assets/images/Hangman2.png")), findsOneWidget);
   });
 
   testWidgets("Left Leg shows up on the page", (WidgetTester tester) async {
-    await tester.pumpWidget(Hangman());
+    await tester.pumpWidget(new Hangman());
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
 
     expect(find.image(const AssetImage("assets/images/Hangman3.png")), findsOneWidget);
   });
 
   testWidgets("Right Leg shows up on the page", (WidgetTester tester) async {
-    await tester.pumpWidget(Hangman());
+    await tester.pumpWidget(new Hangman());
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
 
     expect(find.image(const AssetImage("assets/images/Hangman4.png")), findsOneWidget);
   });
 
   testWidgets("Left Arm shows up on the page", (WidgetTester tester) async {
-    await tester.pumpWidget(Hangman());
+    await tester.pumpWidget(new Hangman());
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
 
-    expect(find.image(const AssetImage("assets/Hangman5.png")), findsOneWidget);
+    expect(find.image(const AssetImage("assets/images/Hangman5.png")), findsOneWidget);
   });
 
   testWidgets("Right Arm shows up on the page", (WidgetTester tester) async {
-    await tester.pumpWidget(Hangman());
+    await tester.pumpWidget(new Hangman());
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
-    await tester.tap(find.text('Z'));
+    await tester.tap(find.text('G'));
     await tester.pumpAndSettle();
 
     expect(find.image(const AssetImage("assets/images/HangmanFull.png")), findsOneWidget);
